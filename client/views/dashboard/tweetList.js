2cb9086fc743185ab047f605647686cf25d175aa
Muhammed Thanish <mnmtanish@gmail.com>
2014-04-12 13:09:11 +0530

    Order by upcoming variation

--- client/views/dashboard/tweetList.js
+++ client/views/dashboard/tweetList.js
@@ -1,7 +1,25 @@
 Template.tweetsList.helpers({
-  tweets: function () {
-    if (Meteor.userId()) {
-      return Tweets.find({userId: Meteor.userId()});
-    };
-  }
+  tweets: getTweets
 });
+
+function getTweets () {
+  if (Meteor.userId()) {
+    var tweets = Tweets.find({userId: Meteor.userId()}).fetch();
+    console.log(JSON.stringify(tweets));
+    tweets.sort(hasLatestVariation);
+    console.log(JSON.stringify(tweets));
+    return tweets;
+  };
+}
+
+function hasLatestVariation (t1, t2) {
+  return (getLatest(t1.variations) < getLatest(t2.variations)) ? -1 : 1;
+}
+
+function getLatest(variations) {
+  var recent = Number.POSITIVE_INFINITY;
+  for(var i=variations.length;i-->0;)
+    if(variations[i].enabled && variations[i].time < latest)
+      recent = variations[i].time;
+  return latest;
+}
