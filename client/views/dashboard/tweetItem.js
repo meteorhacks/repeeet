dbb2b63e3e3ea4f98636b929b3cd9042d5f8205b
Muhammed Thanish <mnmtanish@gmail.com>
2014-04-12 12:05:19 +0530

    Switch between visible variation

--- client/views/dashboard/tweetItem.js
+++ client/views/dashboard/tweetItem.js
@@ -1,15 +1,14 @@
+
 Template.tweetItem.helpers({
-  toDay: Helpers.getDay
+  toDay: Helpers.getDay,
+  text: getDisplayText,
+  variations: getVariations,
 });
 
 Template.tweetItem.events({
-  'click .variation-time-button': showVariation,
-  'click .tweet-delete': deleteTweet
+  'click .tweet-delete': deleteTweet,
+  'click .variation-time-button': setVisibleVariation
 });
-
-function showVariation (e) {
-  alert(JSON.stringify(this));
-}
 
 function deleteTweet (e) {
   $(e.target).parents('.modal').modal('hide');
@@ -18,4 +17,22 @@ function deleteTweet (e) {
   Meteor.setTimeout(function () {
     Meteor.call('remove', self._id);
   }, 1000);
+}
+
+function getDisplayText () {
+  console.log('GET', this);
+  return Session.get(this._id) || this.defaultText;
+}
+
+function getVariations () {
+  var variations = this.variations;
+  for(var i=variations.length; i-->0;) {
+    variations[i].__index = i;
+    variations[i].__tweet = this;
+  }
+  return variations;
+}
+
+function setVisibleVariation (e) {
+  Session.set(this.__tweet._id, this.text || this.__tweet.defaultText);
 }
